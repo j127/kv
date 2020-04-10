8c11d5c4345a399375de843166cd9c74f938fdfa
J. Cohen <hostelmarketing@gmail.com>
2020-04-10 14:25:04 -0700

    Refactor test to use 'setup'

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -4,8 +4,12 @@ defmodule KV.BucketTest do
   # etc.).
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link([])
+    %{bucket: bucket}
+  end
+
+  test "stores values by key" do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
